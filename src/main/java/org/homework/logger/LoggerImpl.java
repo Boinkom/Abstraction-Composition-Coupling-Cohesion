d6cf5f1746ef94912411380e81a40ade382aa0c4
Boinkom <steamnagib@gmail.com>
2024-08-22 13:27:50 +0300

    Fixing logger versions error

--- src/main/java/org/homework/logger/LoggerImpl.java
+++ src/main/java/org/homework/logger/LoggerImpl.java
@@ -3,21 +3,24 @@ package org.homework.logger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LoggerImpl {
+public class LoggerImpl implements org.homework.logger.Logger {
     private final Logger logger;
 
     public LoggerImpl(Class<?> clazz) {
         this.logger = LoggerFactory.getLogger(clazz);
     }
 
+    @Override
     public void info(String message) {
         logger.info(message);
     }
 
+    @Override
     public void debug(String message) {
         logger.debug(message);
     }
 
+    @Override
     public void error(String message, Throwable throwable) {
         logger.error(message, throwable);
     }
